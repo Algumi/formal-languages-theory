7ad0934fe4f68fbff59eee6955ba9b938a9a2b8e
Alex <alex_jel@list.ru>
2019-10-01 19:08:03 +0400

    Added automation parameters and basic realization

--- task_1.py
+++ task_1.py
@@ -17,4 +17,32 @@ class Automatic:
 
     def max_string(self, string, l):
         i, m, flag = l, 0, False
+        self.current_state = self.s
+        for i, char in enumerate(string):
+            self.change_state(char)
+            if self.current_state in self.f:
+                return flag, i - l + 1
+
+
+def test():
+    q = ["empty", "sign", "int", "fract", "e_int", "e_int_num", "e_int_sign", "e_fract",
+         "e_fract_num", "e_fract_sign", "dot"]
+
+    e = ["inp_num", "inp_sign", "inp_e", "inp_dot"]
+    b = {("empty", "inp_sign"): "sign", ("empty", "inp_num"): "int", ("empty", "inp_dot"): "dot",
+         ("sign", "inp_num"): "int", ("sign", "inp_dot"): "dot",
+         ("int", "inp_dot"): "dot", ("int", "inp_num"): "int", ("int", "inp_e"): "e_int",
+         ("dot", "inp_num"): "fract", ("dot", "inp_e"): "e_fract",
+         ("fract", "inp_num"): "fract", ("fract", "inp_e"): "e_fract",
+         ("e_int", "inp_num"): "e_int_num", ("e_int", "inp_sign"): "e_int_sign",
+         ("e_int_sign", "inp_num"): "e_int_num",
+         ("e_int_num", "inp_num"): "e_int_num", ("e_int_num", "inp_dot"): "dot",
+         ("e_fract", "inp_num"): "e_fract_num", ("e_fract", "inp_sign"): "e_fract_sign",
+         ("e_fract_sign", "inp_num"): "e_fract_num",
+         ("e_fract_num", "inp_num"): "e_fract_num"}
+    s = ["empty"]
+    f = ["int", "dot", "fract", "e_int_num", "e_fract_num"]
+    auto = Automatic(q, e, b, s, f)
+    
+
 
